2ad44f042616dc0144eba02e392042fdd2a39f0f
Technerdguy1 <elishakuje12@gmail.com>
2023-10-08 17:42:50 -0700

    2-calloc.c fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,3 +17,23 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, (nmemb * size));
+	return (p);
+}
